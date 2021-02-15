e58d754e897cd7516ae57f63f93b5a7d45b7ab55
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-02-15 12:48:23 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -73,9 +73,9 @@ def delete_all_messages():
  
 def update_all_messages():
     msg = get_readable_message()
-    msg += f"<b>CPU:</b> {psutil.cpu_percent()}%" \
-           f" <b>DISK:</b> {psutil.disk_usage('/').percent}%" \
-           f" <b>RAM:</b> {psutil.virtual_memory().percent}%"
+    msg += f"<b>💻CPU💻:</b> {psutil.cpu_percent()}%" \
+           f" <b>💿DISK💿:</b> {psutil.disk_usage('/').percent}%" \
+           f" <b>💾RAM💾:</b> {psutil.virtual_memory().percent}%"
     with download_dict_lock:
         dlspeed_bytes = 0
         uldl_bytes = 0
