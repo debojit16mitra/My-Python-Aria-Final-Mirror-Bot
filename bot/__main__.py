69fde0ab4e5d293ba8c26bb5b0a49d039c243dbd
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-02-16 09:09:30 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -28,15 +28,15 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>Bot Uptime:</b> {currentTime}\n' \
-            f'<b>Total disk space:</b> {total}\n' \
-            f'<b>Used:</b> {used}  ' \
-            f'<b>Free:</b> {free}\n\n' \
-            f'📊Data Usage📊\n<b>Upload:</b> {sent}\n' \
-            f'<b>Down:</b> {recv}\n\n' \
-            f'<b>CPU:</b> {cpuUsage}% ' \
-            f'<b>RAM:</b> {memory}% ' \
-            f'<b>Disk:</b> {disk}%'
+    stats = f'<b>⌛Bot Uptime:</b> {currentTime}\n' \
+            f'<b>💿Total disk space:</b> {total}\n' \
+            f'<b>😬Used:</b> {used}  ' \
+            f'<b>😌Free:</b> {free}\n\n' \
+            f'📊Data Usage📊\n<b>⬆️Up:</b> {sent}\n' \
+            f'<b>Down⬇️:</b> {recv}\n\n' \
+            f'<b>💻CPU:</b> {cpuUsage}% ' \
+            f'<b>💾RAM:</b> {memory}% ' \
+            f'<b>💿Disk:</b> {disk}%'
     sendMessage(stats, context.bot, update)
  
  
